bd5986c355110a53591cbc08a0a9d98f8c7e598b
Irfan <irfanalamt@gmail.com>
2022-05-13 23:59:40 -0400

    Feat: add postgresql db for customer

--- customer/src/main/java/com/irfanalam/customer/CustomerService.java
+++ customer/src/main/java/com/irfanalam/customer/CustomerService.java
@@ -3,13 +3,14 @@ package com.irfanalam.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
 
-        Customer Customer= com.irfanalam.customer.Customer.builder()
+        Customer customer= Customer.builder()
                 .firstName (request.firstName())
                 .lastName (request.lastName())
                 .email(request.email())
                 .build();
+        customerRepository.save(customer);
     }
 }
